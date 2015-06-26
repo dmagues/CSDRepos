f5ba60af2ddb5e1b760a905786d65d0613d950d1
Jimmy Vasquez <jimmy.vasquez22@gmail.com>
2015-06-26 12:48:52 -0500

    Prueba de Set y Get de Latitud y Clase Coordenada

---
+++ POISSolution/POISSolution/POISTest/TestBuscarPunto.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using POISService;
+
+namespace POISTest
+{
+    [TestClass]
+    public class TestBuscarPunto
+    {
+        [TestMethod]
+        public void CompruebaValorLatitud()
+        {
+            Coordenada Cnds = new Coordenada();
+
+            Cnds.Setlatitud(12345);
+
+            Assert.AreEqual(12345, Cnds.ObtenerLatitud());
+        }
+
+    }
+}

23b232b0ab14d240ea236d67816a587c68809be0
Kevin <holk90@hotmail.com>
2015-06-26 16:55:53 -0500

    Se incluye Funcion de Calculo de Distancia en Metros con su respectivo Test

--- POISSolution/POISSolution/POISService/Coordenada.cs
+++ POISSolution/POISSolution/POISService/Coordenada.cs
@@ -11,7 +11,8 @@ namespace POISService
 
         private double Lat;
         private double Lng;
-        
+
+
 
         public Coordenada  ObtenerCoordenada()
         {
@@ -48,6 +49,25 @@ namespace POISService
         {
             //Formula: Raiz((X2-X1)^2 + (Y2-Y1)^2)
             return Math.Sqrt(Math.Pow((cnd.ObtenerLatitud() - this.Lat),2) + (Math.Pow((cnd.ObtenerLongitud() - this.Lng),2)));
+        }
+
+        public double LatitudAMetros(double p)
+        {
+            return p * 110574.61087757686;
+        }
+
+        public double LongitudAMetros(double p)
+        {
+            return p * 111302.61697430261;
+        }
+
+        public double ObtenerDistanciaMetros(Coordenada cnd)
+        {
+            double x1 = LatitudAMetros(this.Lat);
+            double x2 = LatitudAMetros(cnd.ObtenerLatitud());
+            double y1 = LongitudAMetros(this.Lng);
+            double y2 = LongitudAMetros(cnd.ObtenerLongitud());
+            return Math.Sqrt(Math.Pow((x2 - x1), 2) + (Math.Pow((y2 - y1), 2)));
         }
     }
 }
--- POISSolution/POISSolution/POISTest/TestBuscarPunto.cs
+++ POISSolution/POISSolution/POISTest/TestBuscarPunto.cs
@@ -24,6 +24,23 @@ namespace POISTest
             Assert.AreEqual(lngy1, Cnds.ObtenerLongitud());
         }
 
+
+        [TestMethod]
+        public void TestConversiones()
+        {
+            //Latitud
+            Assert.AreEqual(110574.61087757686, Cnds.LatitudAMetros(1));
+            double latx1 = -74.49;
+            Assert.AreEqual(latx1 * 110574.61087757686, Cnds.LatitudAMetros(latx1));
+            Assert.AreEqual(0, Cnds.LatitudAMetros(0));
+            //Logitud
+            Assert.AreEqual(111302.61697430261, Cnds.LongitudAMetros(1));
+            double lngy1 = -7.6;
+            Assert.AreEqual(lngy1 * 111302.61697430261, Cnds.LongitudAMetros(lngy1));
+            Assert.AreEqual(0, Cnds.LongitudAMetros(0));
+        }
+
+
         [TestMethod]
         public void CompruebaDistancia()
         {
@@ -39,6 +56,16 @@ namespace POISTest
             //Formula: Raiz((X2-X1)^2 + (Y2-Y1)^2)
             double resultado = Math.Sqrt(Math.Pow((latx2 - latx1), 2) + (Math.Pow((lngy2 - lngy1), 2)));
             Assert.AreEqual(resultado, Cnds.ObtenerDistancia(cnds2));
+
+            //Formula: Raiz((X2-X1)^2 + (Y2-Y1)^2)
+            double x1 = Cnds.LatitudAMetros(latx1);
+            double x2 = Cnds.LatitudAMetros(latx2);
+            double y1 = Cnds.LongitudAMetros(lngy1);
+            double y2 = Cnds.LongitudAMetros(lngy2);
+            double metros = Math.Sqrt(Math.Pow((x2 - x1), 2) + (Math.Pow((y2 - y1), 2)));
+            Assert.AreEqual(metros, Cnds.ObtenerDistanciaMetros(cnds2));
+
+
         }
 
     }

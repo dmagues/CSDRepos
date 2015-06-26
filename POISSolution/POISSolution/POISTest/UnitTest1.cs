7ce650f81049ea0673449fa52bcdc67e93aea4ed
carlos <carlosleonardo1@hotmail.com>
2015-06-26 15:45:20 -0500

    new changes

--- POISSolution/POISSolution/POISTest/UnitTest1.cs
+++ POISSolution/POISSolution/POISTest/UnitTest1.cs
@@ -9,20 +9,27 @@ namespace POISTest
     [TestClass]
     public class UnitTest1
     {
+        
         [TestMethod]
         public void TestMethod1()
         {
-            POISService.Service1 servicio = new Service1();
+            var servicio = new Service1();
             var listaCordenadas = servicio.ObtenerPoIsMasCercanos(1, 2, 3);
 
-            var conversion = (Coordenada) listaCordenadas;
-            //foreach (var obj in listaCordenadas)
-            //{
-            //    (List<int>)obj
-            //}
+            Assert.IsNull(listaCordenadas);
+
+            
 
         }
 
+        public void TestMethod2()
+        {
+
+            POISService.Service1 servicio = new Service1();
+            Assert.IsNull(servicio.ObtenerCoordenadas());
+           
+        }
+
     }
-    }
+    
 }

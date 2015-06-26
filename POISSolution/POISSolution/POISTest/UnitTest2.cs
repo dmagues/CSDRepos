a1918adf141c2eb0d4690ac2990c21928f482a95
Daniel Magues <dmagues@hotmail.com>
2015-06-26 14:33:52 -0500

    Nuevas pruebas

--- POISSolution/POISSolution/POISTest/UnitTest2.cs
+++ POISSolution/POISSolution/POISTest/UnitTest2.cs
@@ -1,15 +1,18 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using POISService;
 
 namespace POISTest
 {
     [TestClass]
     public class UnitTest2
     {
+        Service1 servicio = new Service1();
+
         [TestMethod]
         public void TestMethod1()
         {
-
+            Assert.AreEqual(null, servicio.ObtenerPoIsMasCercanos(0, 0, 0));
         }
     }
 }
